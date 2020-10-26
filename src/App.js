f6e1cb13ecd919e573905d28383851b51a00f721
aleksandrov <aaleksandrov@bears.software>
2020-10-26 09:00:36 +0200

    new card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+import './App.css'
+import './reset.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+export const App = () => {
+    let container = useRef(null);
+    let card = useRef(null);
+    let title = useRef(null);
+    let sneaker = useRef(null);
+    let description = useRef(null);
+    let sizes = useRef(null);
+    let purchase = useRef(null);
+
+    useEffect(() => {
+        container.current.addEventListener('mousemove', (e) => {
+            let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+            let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+            card.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        })
+
+        container.current.addEventListener("mouseenter", (e) => {
+            title.current.style.transform = "translateZ(150px)";
+            sneaker.current.style.transform = "translateZ(200px) rotateZ(-45deg)";
+            description.current.style.transform = "translateZ(125px)";
+            sizes.current.style.transform = "translateZ(100px)";
+            purchase.current.style.transform = "translateZ(75px)";
+            card.current.style.transition = "none";
+        });
+
+        container.current.addEventListener("mouseleave", (e) => {
+            card.current.style.transition = "all 0.5s ease";
+            card.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
+            title.current.style.transform = "translateZ(0px)";
+            sneaker.current.style.transform = "translateZ(0px) rotateZ(0deg)";
+            description.current.style.transform = "translateZ(0px)";
+            sizes.current.style.transform = "translateZ(0px)";
+            purchase.current.style.transform = "translateZ(0px)";
+        });
+    }, []);
+
+    return (
+        <div ref={container} className="container">
+            <div ref={card} className="card">
+                <div className="sneaker">
+                    <div className="circle"/>
+                    <img ref={sneaker} src="assets/adidas.png" alt="adidas"/>
+                </div>
+                <div className="info">
+                    <h1 ref={title} className="title">Adidas ZX</h1>
+                    <h3 ref={description}>FUTURE-READY TRAINERS WITH WRAPPED BOOST FOR EXCEPTION COMFORT.</h3>
+                    <div ref={sizes} className="sizes">
+                        <button>39</button>
+                        <button>40</button>
+                        <button className="active">42</button>
+                        <button>44</button>
+                    </div>
+                    <div className="purchase">
+                        <button ref={purchase}>Purchase</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
